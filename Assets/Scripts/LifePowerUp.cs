d605b0e5870ac80d8dc074b919f01de0fb041895
alberto-fuente <afr1004@alu.ubu.es>
2021-01-30 17:22:32 +0200

    heart collectable

---
+++ Assets/Scripts/LifePowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePowerUp : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PickUp(other.gameObject.GetComponent<PlayerController>());
+        }
+    }
+    private void PickUp(PlayerController pc)
+    {
+        if (pc.currentHearts < pc.maxHearts)
+        {
+            pc.currentHearts ++;
+            Destroy(gameObject);
+        }
+
+    }
+}
