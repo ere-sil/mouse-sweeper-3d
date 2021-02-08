d60508fe68f3c7a2e4bf76909b01355c441bc1d2
Samuli Nieminen <samuli.nieminen@edu.turkuamk.fi>
2021-02-08 23:31:29 +0200

    Surrounding mines counter update
    Added counter for surrounding mines. Remade upper minefield level, lost mine positions in the process. Added mine counting children into every upper tile

---
+++ Assets/Scripts/MinesNextToTileCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinesNextToTileCounter : MonoBehaviour
+{
+
+    public int mineValue;
+    private GameObject thisTile;
+    private bool calculateMinesAround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.parent.gameObject.CompareTag("isMine") == false)
+        {
+            calculateMinesAround = true;
+            Debug.Log("this mine child found its parent and went to bed" + calculateMinesAround);
+        }
+        else
+        {
+            gameObject.tag = "calcMine";
+        }
+        Debug.Log("Mine tag = " + gameObject.tag);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (calculateMinesAround == true)
+        {
+            if (other == gameObject.CompareTag("isMine"))
+            {
+                mineValue++;
+            }
+        }
+    }
+
+}
