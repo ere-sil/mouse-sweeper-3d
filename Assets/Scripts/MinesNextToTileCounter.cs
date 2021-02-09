7ede3e5277040f2b58fbab16142385440f9b7bd6
Samuli Nieminen <samuli.nieminen@edu.turkuamk.fi>
2021-02-09 21:52:37 +0200

    Mine Counter, mines, mine highlight, updates
    Build mine counter, updated upper and lower minefields, added mines, added mine highlight key.
    
    Fixes: some levels didnt start, fixed by copying audio manager. Fixed death crash.
    
    Updates some levels mine layout. Mine counter features:
    
    - automatic surrounding-mine-numbering for every tile if tile is not mine
    - adds damaged ground material to tiles that are below upper mine-tile
    - everything is included in tile-prefabs in folder: prefabs/rooms/minefields/top or bottom
    - if you change single tile in those folders you change whole prefab for 9x9 and 12x12- minefields
    - only thing you have to worry about is to chose tiles you want to add as mines, change tag to isMine, and go!

--- Assets/Scripts/MinesNextToTileCounter.cs
+++ Assets/Scripts/MinesNextToTileCounter.cs
@@ -5,24 +5,17 @@ using UnityEngine;
 public class MinesNextToTileCounter : MonoBehaviour
 {
 
-    public int mineValue;
+    private int mineValue;
+    private string mineTextValue;
     private GameObject thisTile;
     private bool calculateMinesAround;
+    public Material explodedGround;
+    private bool ready = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (transform.parent.gameObject.CompareTag("isMine") == false)
-        {
-            calculateMinesAround = true;
-            Debug.Log("this mine child found its parent and went to bed" + calculateMinesAround);
-        }
-        else
-        {
-            gameObject.tag = "calcMine";
-        }
-        Debug.Log("Mine tag = " + gameObject.tag);
-
+        checkTags();
     }
 
     // Update is called once per frame
@@ -33,12 +26,66 @@ public class MinesNextToTileCounter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //This coroutine includes 
+        StartCoroutine(Funcs(other));
+    }
+
+    // this checks if objects parent is mine or is not mine. If mine, changes tag to noCalcMine, and if not tag is calcMine.
+    // The tag determines if the object on the bottom mine will enable either destructed mine sprite or a number that represents the mines around.
+    // mines are calculated if calculateMinesAround == true
+    private void checkTags()
+    {
+        if (transform.parent.gameObject.CompareTag("noMine"))
+        {
+            calculateMinesAround = true;
+            gameObject.tag = "calcMine";
+        }
+        else if (transform.parent.gameObject.CompareTag("isMine"))
+        {
+            calculateMinesAround = false;
+            gameObject.tag = "triggerIsMine";
+            Debug.Log("Found Mine" + gameObject.tag);
+        }
+        else if (transform.parent.gameObject.CompareTag("BottomTile"))
+        {
+            calculateMinesAround = false;
+        }
+    }
+
+
+    private IEnumerator Funcs(Collider other)
+    {
+        yield return new WaitForSeconds(1.5f);
+        calculateMines(other);
+        yield return new WaitForSeconds(2.0f);
+        setNumberOrSprite(other);
+    }
+
+    //calculates mines aroud the tile
+    private void calculateMines(Collider other)
+    {
         if (calculateMinesAround == true)
         {
-            if (other == gameObject.CompareTag("isMine"))
+            if (other.gameObject.CompareTag("triggerIsMine"))
             {
                 mineValue++;
             }
+        }
+    }
+
+    //this script will either set the mine number in to the Text Mesh OR
+    // change material to exploded ground, if colliding with tile marked as mine
+    private void setNumberOrSprite(Collider other)
+    {
+        if (gameObject.CompareTag("MineNumber") & other.gameObject.CompareTag("calcMine"))
+        {
+            mineTextValue = other.gameObject.GetComponent<MinesNextToTileCounter>().mineValue.ToString();
+            GetComponent<TextMesh>().text = mineTextValue;
+        }
+        else if (gameObject.CompareTag("MineNumber") & other.gameObject.CompareTag("triggerIsMine"))
+        {
+            transform.parent.GetComponent<MeshRenderer>().material = explodedGround;
+            gameObject.SetActive(false);
         }
     }
 
